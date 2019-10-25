bf381c6c2d7bffc31e874363f2d5fd495ec23a9d
Divino J. Silva <caiaponianet@hotmail.com>
2019-10-25 01:29:47 -0300

    Validações no backend com uso de Yup

--- src/app/middieware/auth.js
+++ src/app/middieware/auth.js
@@ -2,17 +2,21 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
     return res.status(401).json({ error: 'Token not provider' });
   }
 
-  const [, token] = authHeader.splint(' ');
+  const [, token] = authHeader.split(' ');
 
   try {
-    jwt.verify();
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
   } catch (erro) {
     return res.status(401).json({ error: 'Token invalid' });
   }
