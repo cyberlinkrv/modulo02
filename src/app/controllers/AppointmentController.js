159e0a498f964878139a24e37a2af32a3e64ceff
Divino J. Silva <caiaponianet@hotmail.com>
2020-03-18 16:53:14 -0300

    Validações de agendamento  - library add date-fns for validate dates

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -28,10 +29,40 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with prividers' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    /**
+     * Check for past date
+     */
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        /**
+         * Nome ta coluna no banco ficou
+         * incorreto refere-se a Canceled_id */
+        conceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.user_id,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
