15751856a76dfbd9ba8e7706069cee328b897e19
Divino J. Silva <caiaponianet@hotmail.com>
2020-03-20 11:16:36 -0300

    Marcar notificações como lidas

--- src/routes.js
+++ src/routes.js
@@ -30,6 +30,8 @@ routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
 
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
